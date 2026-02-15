ac99e022c538af045c26b645b77063c5f6d3bd10
Tymoteusz <vubes44@gmail.com>
2026-02-15 10:56:20 +0100

    added </style> closing

--- src/components/molecules/MIconButton.vue
+++ src/components/molecules/MIconButton.vue
@@ -18,16 +18,16 @@
   import AButton from "@atoms/AButton.vue";
   import AIcon from "@atoms/AIcon.vue";
 
-    const props = withDefaults(
-        defineProps<{
-            icon: string;
-            alt: string;
-            isDisabled?: boolean;
-        }>(),
-        {
-            isDisabled: false,
-        }
-    );
+  const props = withDefaults(
+    defineProps<{
+      icon: string;
+      alt: string;
+      isDisabled?: boolean;
+    }>(),
+    {
+      isDisabled: false,
+    }
+  );
 
   const emit = defineEmits<{
     (e: "click"): void;
@@ -37,23 +37,23 @@
     if (props.isDisabled) return;
     emit("click");
   };
-
 </script>
 <style scoped lang="scss">
-    .m-icon-button {
-        padding: 5px;
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-        opacity: 0.7;
-        transition: opacity 0.2s;
-    
-        &:hover {
-        opacity: 1;
-        }
-    
-        &--disabled {
-        opacity: 0.2;
-        cursor: auto;
+  .m-icon-button {
+    padding: 5px;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    opacity: 0.7;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    &--disabled {
+      opacity: 0.2;
+      cursor: auto;
     }
   }
+</style>
